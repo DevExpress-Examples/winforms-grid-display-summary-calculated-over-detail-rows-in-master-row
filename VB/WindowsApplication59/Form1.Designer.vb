991fff27adc2aced1077651cb74a04aee4873613
DevExpressExampleBot <fakeEmail>
2023-09-19 13:25:22 +0400

    Source auto update [skip ci]

--- VB/WindowsApplication59/Form1.Designer.vb
+++ VB/WindowsApplication59/Form1.Designer.vb
@@ -1,178 +1,188 @@
-Imports Microsoft.VisualBasic
-Imports System
 Namespace WindowsApplication59
-	Partial Public Class Form1
-		''' <summary>
-		''' Required designer variable.
-		''' </summary>
-		Private components As System.ComponentModel.IContainer = Nothing
 
-		''' <summary>
-		''' Clean up any resources being used.
-		''' </summary>
-		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-			If disposing AndAlso (components IsNot Nothing) Then
-				components.Dispose()
-			End If
-			MyBase.Dispose(disposing)
-		End Sub
+    Partial Class Form1
 
-		#Region "Windows Form Designer generated code"
+        ''' <summary>
+        ''' Required designer variable.
+        ''' </summary>
+        Private components As System.ComponentModel.IContainer = Nothing
 
-		''' <summary>
-		''' Required method for Designer support - do not modify
-		''' the contents of this method with the code editor.
-		''' </summary>
-		Private Sub InitializeComponent()
-			Me.gridControl1 = New DevExpress.XtraGrid.GridControl()
-			Me.dataSet1 = New System.Data.DataSet()
-			Me.dataTable1 = New System.Data.DataTable()
-			Me.dataColumn1 = New System.Data.DataColumn()
-			Me.dataColumn3 = New System.Data.DataColumn()
-			Me.dataColumn4 = New System.Data.DataColumn()
-			Me.gridView1 = New DevExpress.XtraGrid.Views.Grid.GridView()
-			Me.dataTable2 = New System.Data.DataTable()
-			Me.dataColumn5 = New System.Data.DataColumn()
-			Me.dataColumn6 = New System.Data.DataColumn()
-			Me.dataColumn7 = New System.Data.DataColumn()
-			Me.colID = New DevExpress.XtraGrid.Columns.GridColumn()
-			Me.colName = New DevExpress.XtraGrid.Columns.GridColumn()
-			Me.colBudget = New DevExpress.XtraGrid.Columns.GridColumn()
-			CType(Me.gridControl1, System.ComponentModel.ISupportInitialize).BeginInit()
-			CType(Me.dataSet1, System.ComponentModel.ISupportInitialize).BeginInit()
-			CType(Me.dataTable1, System.ComponentModel.ISupportInitialize).BeginInit()
-			CType(Me.gridView1, System.ComponentModel.ISupportInitialize).BeginInit()
-			CType(Me.dataTable2, System.ComponentModel.ISupportInitialize).BeginInit()
-			Me.SuspendLayout()
-			' 
-			' gridControl1
-			' 
-			Me.gridControl1.DataMember = "Table1"
-			Me.gridControl1.DataSource = Me.dataSet1
-			Me.gridControl1.Dock = System.Windows.Forms.DockStyle.Fill
-			Me.gridControl1.EmbeddedNavigator.Name = ""
-			Me.gridControl1.Location = New System.Drawing.Point(0, 0)
-			Me.gridControl1.MainView = Me.gridView1
-			Me.gridControl1.Name = "gridControl1"
-			Me.gridControl1.Size = New System.Drawing.Size(423, 266)
-			Me.gridControl1.TabIndex = 0
-			Me.gridControl1.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() { Me.gridView1})
-			' 
-			' dataSet1
-			' 
-			Me.dataSet1.DataSetName = "NewDataSet"
-			Me.dataSet1.Relations.AddRange(New System.Data.DataRelation() { New System.Data.DataRelation("Project_Tasks", "Table1", "Table2", New String() { "ID"}, New String() { "Project"}, False)})
-			Me.dataSet1.Tables.AddRange(New System.Data.DataTable() { Me.dataTable1, Me.dataTable2})
-			' 
-			' dataTable1
-			' 
-			Me.dataTable1.Columns.AddRange(New System.Data.DataColumn() { Me.dataColumn1, Me.dataColumn3, Me.dataColumn4})
-			Me.dataTable1.Constraints.AddRange(New System.Data.Constraint() { New System.Data.UniqueConstraint("Constraint2", New String() { "ID"}, True)})
-			Me.dataTable1.PrimaryKey = New System.Data.DataColumn() { Me.dataColumn1}
-			Me.dataTable1.TableName = "Table1"
-			' 
-			' dataColumn1
-			' 
-			Me.dataColumn1.AllowDBNull = False
-			Me.dataColumn1.AutoIncrement = True
-			Me.dataColumn1.ColumnName = "ID"
-			Me.dataColumn1.DataType = GetType(Integer)
-			' 
-			' dataColumn3
-			' 
-			Me.dataColumn3.ColumnName = "Name"
-			' 
-			' dataColumn4
-			' 
-			Me.dataColumn4.ColumnName = "Budget"
-			Me.dataColumn4.DataType = GetType(Decimal)
-			' 
-			' gridView1
-			' 
-			Me.gridView1.Columns.AddRange(New DevExpress.XtraGrid.Columns.GridColumn() { Me.colID, Me.colName, Me.colBudget})
-			Me.gridView1.GridControl = Me.gridControl1
-			Me.gridView1.Name = "gridView1"
-			' 
-			' dataTable2
-			' 
-			Me.dataTable2.Columns.AddRange(New System.Data.DataColumn() { Me.dataColumn5, Me.dataColumn6, Me.dataColumn7})
-			Me.dataTable2.Constraints.AddRange(New System.Data.Constraint() { New System.Data.UniqueConstraint("Constraint1", New String() { "ID"}, True), New System.Data.ForeignKeyConstraint("Relation1", "Table1", New String() { "ID"}, New String() { "Project"}, System.Data.AcceptRejectRule.None, System.Data.Rule.Cascade, System.Data.Rule.Cascade)})
-			Me.dataTable2.PrimaryKey = New System.Data.DataColumn() { Me.dataColumn5}
-			Me.dataTable2.TableName = "Table2"
-			' 
-			' dataColumn5
-			' 
-			Me.dataColumn5.AllowDBNull = False
-			Me.dataColumn5.AutoIncrement = True
-			Me.dataColumn5.ColumnName = "ID"
-			Me.dataColumn5.DataType = GetType(Integer)
-			' 
-			' dataColumn6
-			' 
-			Me.dataColumn6.AllowDBNull = False
-			Me.dataColumn6.ColumnName = "Project"
-			Me.dataColumn6.DataType = GetType(Integer)
-			' 
-			' dataColumn7
-			' 
-			Me.dataColumn7.ColumnName = "Description"
-			' 
-			' colID
-			' 
-			Me.colID.Caption = "ID"
-			Me.colID.FieldName = "ID"
-			Me.colID.Name = "colID"
-			' 
-			' colName
-			' 
-			Me.colName.Caption = "Name"
-			Me.colName.FieldName = "Name"
-			Me.colName.Name = "colName"
-			Me.colName.Visible = True
-			Me.colName.VisibleIndex = 0
-			' 
-			' colBudget
-			' 
-			Me.colBudget.Caption = "Budget"
-			Me.colBudget.FieldName = "Budget"
-			Me.colBudget.Name = "colBudget"
-			Me.colBudget.Visible = True
-			Me.colBudget.VisibleIndex = 1
-			' 
-			' Form1
-			' 
-			Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
-			Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-			Me.ClientSize = New System.Drawing.Size(423, 266)
-			Me.Controls.Add(Me.gridControl1)
-			Me.Name = "Form1"
-			Me.Text = "Form1"
-			CType(Me.gridControl1, System.ComponentModel.ISupportInitialize).EndInit()
-			CType(Me.dataSet1, System.ComponentModel.ISupportInitialize).EndInit()
-			CType(Me.dataTable1, System.ComponentModel.ISupportInitialize).EndInit()
-			CType(Me.gridView1, System.ComponentModel.ISupportInitialize).EndInit()
-			CType(Me.dataTable2, System.ComponentModel.ISupportInitialize).EndInit()
-			Me.ResumeLayout(False)
+        ''' <summary>
+        ''' Clean up any resources being used.
+        ''' </summary>
+        ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            If disposing AndAlso (Me.components IsNot Nothing) Then
+                Me.components.Dispose()
+            End If
 
-		End Sub
+            MyBase.Dispose(disposing)
+        End Sub
 
-		#End Region
+#Region "Windows Form Designer generated code"
+        ''' <summary>
+        ''' Required method for Designer support - do not modify
+        ''' the contents of this method with the code editor.
+        ''' </summary>
+        Private Sub InitializeComponent()
+            Me.gridControl1 = New DevExpress.XtraGrid.GridControl()
+            Me.dataSet1 = New System.Data.DataSet()
+            Me.dataTable1 = New System.Data.DataTable()
+            Me.dataColumn1 = New System.Data.DataColumn()
+            Me.dataColumn3 = New System.Data.DataColumn()
+            Me.dataColumn4 = New System.Data.DataColumn()
+            Me.gridView1 = New DevExpress.XtraGrid.Views.Grid.GridView()
+            Me.dataTable2 = New System.Data.DataTable()
+            Me.dataColumn5 = New System.Data.DataColumn()
+            Me.dataColumn6 = New System.Data.DataColumn()
+            Me.dataColumn7 = New System.Data.DataColumn()
+            Me.colID = New DevExpress.XtraGrid.Columns.GridColumn()
+            Me.colName = New DevExpress.XtraGrid.Columns.GridColumn()
+            Me.colBudget = New DevExpress.XtraGrid.Columns.GridColumn()
+            CType((Me.gridControl1), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.dataSet1), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.dataTable1), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.gridView1), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.dataTable2), System.ComponentModel.ISupportInitialize).BeginInit()
+            Me.SuspendLayout()
+            ' 
+            ' gridControl1
+            ' 
+            Me.gridControl1.DataMember = "Table1"
+            Me.gridControl1.DataSource = Me.dataSet1
+            Me.gridControl1.Dock = System.Windows.Forms.DockStyle.Fill
+            Me.gridControl1.EmbeddedNavigator.Name = ""
+            Me.gridControl1.Location = New System.Drawing.Point(0, 0)
+            Me.gridControl1.MainView = Me.gridView1
+            Me.gridControl1.Name = "gridControl1"
+            Me.gridControl1.Size = New System.Drawing.Size(423, 266)
+            Me.gridControl1.TabIndex = 0
+            Me.gridControl1.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() {Me.gridView1})
+            ' 
+            ' dataSet1
+            ' 
+            Me.dataSet1.DataSetName = "NewDataSet"
+            Me.dataSet1.Relations.AddRange(New System.Data.DataRelation() {New System.Data.DataRelation("Project_Tasks", "Table1", "Table2", New String() {"ID"}, New String() {"Project"}, False)})
+            Me.dataSet1.Tables.AddRange(New System.Data.DataTable() {Me.dataTable1, Me.dataTable2})
+            ' 
+            ' dataTable1
+            ' 
+            Me.dataTable1.Columns.AddRange(New System.Data.DataColumn() {Me.dataColumn1, Me.dataColumn3, Me.dataColumn4})
+            Me.dataTable1.Constraints.AddRange(New System.Data.Constraint() {New System.Data.UniqueConstraint("Constraint2", New String() {"ID"}, True)})
+            Me.dataTable1.PrimaryKey = New System.Data.DataColumn() {Me.dataColumn1}
+            Me.dataTable1.TableName = "Table1"
+            ' 
+            ' dataColumn1
+            ' 
+            Me.dataColumn1.AllowDBNull = False
+            Me.dataColumn1.AutoIncrement = True
+            Me.dataColumn1.ColumnName = "ID"
+            Me.dataColumn1.DataType = GetType(Integer)
+            ' 
+            ' dataColumn3
+            ' 
+            Me.dataColumn3.ColumnName = "Name"
+            ' 
+            ' dataColumn4
+            ' 
+            Me.dataColumn4.ColumnName = "Budget"
+            Me.dataColumn4.DataType = GetType(Decimal)
+            ' 
+            ' gridView1
+            ' 
+            Me.gridView1.Columns.AddRange(New DevExpress.XtraGrid.Columns.GridColumn() {Me.colID, Me.colName, Me.colBudget})
+            Me.gridView1.GridControl = Me.gridControl1
+            Me.gridView1.Name = "gridView1"
+            ' 
+            ' dataTable2
+            ' 
+            Me.dataTable2.Columns.AddRange(New System.Data.DataColumn() {Me.dataColumn5, Me.dataColumn6, Me.dataColumn7})
+            Me.dataTable2.Constraints.AddRange(New System.Data.Constraint() {New System.Data.UniqueConstraint("Constraint1", New String() {"ID"}, True), New System.Data.ForeignKeyConstraint("Relation1", "Table1", New String() {"ID"}, New String() {"Project"}, System.Data.AcceptRejectRule.None, System.Data.Rule.Cascade, System.Data.Rule.Cascade)})
+            Me.dataTable2.PrimaryKey = New System.Data.DataColumn() {Me.dataColumn5}
+            Me.dataTable2.TableName = "Table2"
+            ' 
+            ' dataColumn5
+            ' 
+            Me.dataColumn5.AllowDBNull = False
+            Me.dataColumn5.AutoIncrement = True
+            Me.dataColumn5.ColumnName = "ID"
+            Me.dataColumn5.DataType = GetType(Integer)
+            ' 
+            ' dataColumn6
+            ' 
+            Me.dataColumn6.AllowDBNull = False
+            Me.dataColumn6.ColumnName = "Project"
+            Me.dataColumn6.DataType = GetType(Integer)
+            ' 
+            ' dataColumn7
+            ' 
+            Me.dataColumn7.ColumnName = "Description"
+            ' 
+            ' colID
+            ' 
+            Me.colID.Caption = "ID"
+            Me.colID.FieldName = "ID"
+            Me.colID.Name = "colID"
+            ' 
+            ' colName
+            ' 
+            Me.colName.Caption = "Name"
+            Me.colName.FieldName = "Name"
+            Me.colName.Name = "colName"
+            Me.colName.Visible = True
+            Me.colName.VisibleIndex = 0
+            ' 
+            ' colBudget
+            ' 
+            Me.colBudget.Caption = "Budget"
+            Me.colBudget.FieldName = "Budget"
+            Me.colBudget.Name = "colBudget"
+            Me.colBudget.Visible = True
+            Me.colBudget.VisibleIndex = 1
+            ' 
+            ' Form1
+            ' 
+            Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
+            Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+            Me.ClientSize = New System.Drawing.Size(423, 266)
+            Me.Controls.Add(Me.gridControl1)
+            Me.Name = "Form1"
+            Me.Text = "Form1"
+            CType((Me.gridControl1), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me.dataSet1), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me.dataTable1), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me.gridView1), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me.dataTable2), System.ComponentModel.ISupportInitialize).EndInit()
+            Me.ResumeLayout(False)
+        End Sub
 
-		Private gridControl1 As DevExpress.XtraGrid.GridControl
-		Private gridView1 As DevExpress.XtraGrid.Views.Grid.GridView
-		Private dataSet1 As System.Data.DataSet
-		Private dataTable1 As System.Data.DataTable
-		Private dataColumn1 As System.Data.DataColumn
-		Private dataColumn3 As System.Data.DataColumn
-		Private dataColumn4 As System.Data.DataColumn
-		Private dataTable2 As System.Data.DataTable
-		Private dataColumn5 As System.Data.DataColumn
-		Private dataColumn6 As System.Data.DataColumn
-		Private dataColumn7 As System.Data.DataColumn
-		Private colID As DevExpress.XtraGrid.Columns.GridColumn
-		Private colName As DevExpress.XtraGrid.Columns.GridColumn
-		Private colBudget As DevExpress.XtraGrid.Columns.GridColumn
-	End Class
+#End Region
+        Private gridControl1 As DevExpress.XtraGrid.GridControl
+
+        Private gridView1 As DevExpress.XtraGrid.Views.Grid.GridView
+
+        Private dataSet1 As System.Data.DataSet
+
+        Private dataTable1 As System.Data.DataTable
+
+        Private dataColumn1 As System.Data.DataColumn
+
+        Private dataColumn3 As System.Data.DataColumn
+
+        Private dataColumn4 As System.Data.DataColumn
+
+        Private dataTable2 As System.Data.DataTable
+
+        Private dataColumn5 As System.Data.DataColumn
+
+        Private dataColumn6 As System.Data.DataColumn
+
+        Private dataColumn7 As System.Data.DataColumn
+
+        Private colID As DevExpress.XtraGrid.Columns.GridColumn
+
+        Private colName As DevExpress.XtraGrid.Columns.GridColumn
+
+        Private colBudget As DevExpress.XtraGrid.Columns.GridColumn
+    End Class
 End Namespace
-
--- VB/WindowsApplication59/Form1.vb
+++ VB/WindowsApplication59/Form1.vb
@@ -1,52 +1,45 @@
-Imports Microsoft.VisualBasic
-Imports System
-Imports System.Collections.Generic
 Imports System.Collections
 Imports System.ComponentModel
 Imports System.Data
 Imports System.Drawing
-Imports System.Text
 Imports System.Windows.Forms
 Imports DevExpress.XtraGrid.Views.Grid
 Imports DevExpress.XtraGrid.Columns
 
 Namespace WindowsApplication59
-	Partial Public Class Form1
-		Inherits Form
-		Public Sub New()
-			InitializeComponent()
-			InitData()
 
-			gridControl1.UseEmbeddedNavigator = True
-			gridControl1.ForceInitialize()
-			Dim col As GridColumn = gridView1.Columns.AddField("Tasks")
-			col.UnboundType = DevExpress.Data.UnboundColumnType.Integer
-			col.Visible = True
-			AddHandler gridView1.CustomUnboundColumnData, AddressOf gridView1_CustomUnboundColumnData
-		End Sub
+    Public Partial Class Form1
+        Inherits Form
 
-		Private Sub gridView1_CustomUnboundColumnData(ByVal sender As Object, ByVal e As DevExpress.XtraGrid.Views.Base.CustomColumnDataEventArgs)
-			Dim view As GridView = TryCast(sender, GridView)
-			If e.Column.FieldName <> "Tasks" Then
-				Return
-			End If
-			If (Not e.IsGetData) Then
-				Return
-			End If
-			Dim row As DataRow = (TryCast((TryCast(view.DataSource, IList))(e.ListSourceRowIndex), DataRowView)).Row
-			e.Value = row.GetChildRows("Project_Tasks").Length
-		End Sub
+        Public Sub New()
+            InitializeComponent()
+            InitData()
+            gridControl1.UseEmbeddedNavigator = True
+            gridControl1.ForceInitialize()
+            Dim col As GridColumn = gridView1.Columns.AddField("Tasks")
+            col.UnboundType = DevExpress.Data.UnboundColumnType.Integer
+            col.Visible = True
+            AddHandler gridView1.CustomUnboundColumnData, New DevExpress.XtraGrid.Views.Base.CustomColumnDataEventHandler(AddressOf gridView1_CustomUnboundColumnData)
+        End Sub
 
-		Private Sub InitData()
-			dataTable1.Rows.Add(New Object() { 0, "Software", 4000 })
-			dataTable1.Rows.Add(New Object() { 1, "Hardware", 1500 })
-			dataTable1.Rows.Add(New Object() { 2, "Project X", 100000 })
-			dataTable2.Rows.Add(New Object() { Nothing, 0, "Drivers" })
-			dataTable2.Rows.Add(New Object() { Nothing, 0, "Application" })
-			dataTable2.Rows.Add(New Object() { Nothing, 1, "All the stuff" })
-			dataTable2.Rows.Add(New Object() { Nothing, 2, "Part X" })
-			dataTable2.Rows.Add(New Object() { Nothing, 2, "Part Y" })
-			dataTable2.Rows.Add(New Object() { Nothing, 2, "Part Z" })
-		End Sub
-	End Class
+        Private Sub gridView1_CustomUnboundColumnData(ByVal sender As Object, ByVal e As DevExpress.XtraGrid.Views.Base.CustomColumnDataEventArgs)
+            Dim view As GridView = TryCast(sender, GridView)
+            If Not Equals(e.Column.FieldName, "Tasks") Then Return
+            If Not e.IsGetData Then Return
+            Dim row As DataRow = TryCast(TryCast(view.DataSource, IList)(e.ListSourceRowIndex), DataRowView).Row
+            e.Value = row.GetChildRows("Project_Tasks").Length
+        End Sub
+
+        Private Sub InitData()
+            dataTable1.Rows.Add(New Object() {0, "Software", 4000})
+            dataTable1.Rows.Add(New Object() {1, "Hardware", 1500})
+            dataTable1.Rows.Add(New Object() {2, "Project X", 100000})
+            dataTable2.Rows.Add(New Object() {Nothing, 0, "Drivers"})
+            dataTable2.Rows.Add(New Object() {Nothing, 0, "Application"})
+            dataTable2.Rows.Add(New Object() {Nothing, 1, "All the stuff"})
+            dataTable2.Rows.Add(New Object() {Nothing, 2, "Part X"})
+            dataTable2.Rows.Add(New Object() {Nothing, 2, "Part Y"})
+            dataTable2.Rows.Add(New Object() {Nothing, 2, "Part Z"})
+        End Sub
+    End Class
 End Namespace
